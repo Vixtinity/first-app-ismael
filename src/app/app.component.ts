e59f3642ca0c89dbbddbdd4e90c8b3b6f41a45a0
Ismael <08062006ismael@gmail.com>
2025-04-14 17:47:39 +0200

    IFL Punto 10. Add routing: -Configuracion de las rutas de las paginas y tambien he creado detalles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-root',
   
-  imports: [HomeComponent, HousingLocationComponent],
+  imports: [HomeComponent, HousingLocationComponent, RouterOutlet, RouterLink],
   template: `
         <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
